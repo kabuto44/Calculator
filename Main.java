09e0d7fd065a3b8a45e28cbaee7ef8addf574e7d
kabuto44 <90492587+kabuto44@users.noreply.github.com>
2021-09-11 11:29:13 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,12 @@
 // Calculator
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+		Calculator a = new Calculator();
+		Scanner b = new Scanner(System.in);
+		System.out.println("Input equation");
+		a.setSequence(b.nextLine());
+		a.compile();
+  }
+}
